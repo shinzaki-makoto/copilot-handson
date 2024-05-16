61b9046b9393a1a0dbbcaea3c4f5fb0e1be83847
shinzaki-makoto <100187347+shinzaki-makoto@users.noreply.github.com>
2024-05-16 09:45:34 +0000

    Copilot second commit

---
+++ app.py
@@ -0,0 +1,10 @@
+def hello():
+    return "Hello World!"
+
+def prime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
